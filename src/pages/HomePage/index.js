1bfc123aa11fb4851872085de9d95ecba8ee82e3
Ezgi Bas <basezgi@gmail.com>
2021-12-04 17:15:18 -0500

    added buttons to header and footer

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+function getData(){
+}
 
 function HomePage() {
-  return <div>home</div>;
+  const [eventData, setEventData] = useState()
+  useEffect(() => {
+    fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      setEventData(data.link)
+    });
+  });
+  return <div>The title is: {eventData} </div>;
 }
 
 export default HomePage;
---
+++ src/practice/ezgi/practice.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Welcome(props) {
+    return <h1>hello {props.name}</h1>
+}
+
+// props = {name: "Britney"}
+
+// useState() -> returns state and function to update state
+function listOfEvents(props) {
+
+    return <h1></h1>
+
+}
