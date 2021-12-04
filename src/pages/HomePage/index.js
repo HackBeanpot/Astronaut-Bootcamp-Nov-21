07ec1ac01b70b6f3ec7a7ca4054fc32fffca82cb
Megan Liu <meganliu@Megans-MacBook.local>
2021-12-04 17:34:17 -0500

    everything

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function HomePage() {
-  return <div>home</div>;
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+function BasicButtons() {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
+}
+// function getData(){}
+
+export default function HomePage() {
+  const [textToBeDisplayed, setTextToBeDisplayed] = useState('loading...');
+  useEffect(() => {
+    fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        //data = {title :'', link: '', events: []}
+        setTextToBeDisplayed(data.title);
+      });
+  });
+  return <div>Environmental Events Currently Occurring in the World : {textToBeDisplayed} </div>;
 }
 
-export default HomePage;
+//export default HomePage;
+
+
+//functional components RETURN how to draw <div> <div> <div>
+
+//useState keeps track of your state, actual_value, functionToUpdateYourValue
+//useEffect just does something, fetches nasa, turns into an object, and sets our state :) 
