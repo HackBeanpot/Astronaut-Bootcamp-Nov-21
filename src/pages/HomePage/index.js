c4403e84eb29f4ff422d1f9cf72297f2133d03c8
AvRajath <rajathav28@gmail.com>
2021-12-04 14:19:49 -0500

    First change

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+function getData() {
+  fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events")
+  .then(response => {
+    return response.json();
+  }).then(data => {
+     return data.title;
+  })
+}
 function HomePage() {
-  return <div>home</div>;
+  const [eventData, setEventData] = useState()
+  useEffect(() => {
+    fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events")
+  .then(response => {
+    return response.json();
+  }).then(data => {
+     setEventData(data.title);
+  });
+});
+  return <div>Hello World {eventData}</div>;
 }
 
 export default HomePage;
