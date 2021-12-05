4c56889b4d1c4d8b18d6a46db5327c751f92abc4
Ezgi Bas <basezgi@gmail.com>
2021-12-05 00:09:15 -0500

    added card to Risks, image not reendering

--- src/pages/Risk/index.js
+++ src/pages/Risk/index.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
+import { makeStyles } from "@mui/styles";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
 
+const useStyles = makeStyles({
+  header: {
+    backgroundColor: "rgb(50,50,80)",
+    height: "10vh",
+  },
+  button: {
+    margin: 10,
+  },
+});
+
+function MakeCard(props) {
+  const classes = useStyles();
+  return (
+      <Card>
+        <CardMedia
+          style={{ height: 100, width: 200, paddingTop: "56%" }}
+          alt = "puppy"
+          component="img"
+          image="./puppy.jpg"
+        />
+        <CardContent>Card Content</CardContent>
+      </Card>
+  );
+}
 
 function RiskPage() {
-    return(<div>risk</div>);
+  const classes = useStyles();
+  return (
+    <div>
+      <MakeCard></MakeCard>
+    </div>
+  );
 }
 
 export default RiskPage;
